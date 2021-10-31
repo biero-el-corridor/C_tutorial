6859a4f5eabc725012f941f3009ec833f01cb14d
biero-el-corridor <85730394+biero-el-corridor@users.noreply.github.com>
2021-10-31 20:02:38 +0100

    add pointer use
    add of exemple for pointer use

--- exemple_pointer.c
+++ exemple_pointer.c
@@ -5,9 +5,26 @@ int main(){
 
     int a = 5; 
     int *PointerToA;
+    char* hello =  "azer";
+    char arrai[5] = "world";
 
+    //le pointeur et egal a la valeur de l'emplacement 
     PointerToA = &a;
+    
+    char *PointeurToArrai = arrai; 
+    
+    //va afficher le contenue de a (5 ducoup)
+    printf("%d \n",*PointerToA);
 
-    printf("%d",*PointerToA);
+    //va afficher l'emplacmeent mémoire de PointerToA 
+    printf("%d\n",&PointerToA);
+
+    //print de la value de hello 
+    printf("%s\n",hello);
+    
+    //print d'un array a partir d'un pointeur
+    printf("%s",PointeurToArrai);
+    
     return 0; 
+
 }
