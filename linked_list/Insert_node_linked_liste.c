c1cde99fd71c696a83954d4faaabdac1ab22262f
biero-el-corridor <85730394+biero-el-corridor@users.noreply.github.com>
2021-11-07 11:46:46 +0100

    add exemple
    add the begening of add node in the middle of the linked list

--- linked_list/Insert_node_linked_liste.c
+++ linked_list/Insert_node_linked_liste.c
@@ -9,13 +9,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 struct Node{
-
     //définitions de la donnée 
     int data; 
     //pointe sur le futur emplacmeent via le pointeur
     struct Node *next; 
 };
-
 
 //fonctions de print du contenue de la list chainée
 int PrintList(struct Node* n){
@@ -26,7 +24,6 @@ int PrintList(struct Node* n){
         //on passe a la prochaine liste via le pointeur de fin 
         n = n->next; 
     }
-    
 }
 
 //ajout de la jonctions pour ajouter un noeud (liste) en début de chaine
@@ -43,19 +40,45 @@ void push(struct Node** head_ref, int new_data){
     new_node->next = (*head_ref); 
 
     //je ne cmprend pas celuis la 
-    (*head_ref) = new_node; 
+    //(*head_ref) = new_node; 
 }
+
+
+void insertAfter(struct Node* prev_node, int new_data){
+
+    //verifications if the node pointe or have the NULL value 
+    //cwe whont whant the null value because that mean this is the last list 
+    if(prev_node == NULL){
+        printf("whe whant to insert in the middle not at the end bro"); 
+        return;
+    }
+    //alocations of memory area for each iterations of new_node 
+    //definitions of the size of new_node 
+    struct Node *new_node = (struct Node*) malloc(sizeof(struct Node)); 
+
+    //attributions of data in the new node
+    new_node->data = new_data; 
+
+    //the new node point to the previous node
+    new_node->next = prev_node->next; 
+
+    //prev_node = new_node->next; 
+    prev_node->next = new_node; 
+}
+
+
+
+
 
 int main(){
     //créations d'une structure qui ne vaut rien 
     struct Node* head = NULL; 
-
     //apelle de la fontions push 
     //le 1er agument pointe vers le numero de l'emplacment mémoire de head
-    //
+    //le deuxiéme argument et la valeur de data
     push(&head,1); 
+    insertAfter(head->next,4); 
     push(&head,2); 
     push(&head,3); 
-
     PrintList(head); 
 }
